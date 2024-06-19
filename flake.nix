19546e572ffb2cc14ea83884b1374b7804c38a1c
bwsdm <2353927+bwsdm@users.noreply.github.com>
2024-06-18 20:20:47 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Example kickstart NixOS desktop environment.";
+
+  inputs = {
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    #nixvim = {
+      #url = "github:nix-community/nixvim/nixos-24.05";
+      #inputs.nixpkgs.follows = "nixpkgs";
+    #};
+  };
+
+  outputs = inputs @ {
+    self,
+    home-manager,
+    nixpkgs,
+    #nixvim,
+    ...
+  }: let
+    nixos-system = import ./system/nixos.nix {
+      inherit inputs;
+      username = "bwsdm"; # TODO: replace with user name and remove throw
+      password = ""; # TODO: replace with password and remove throw
+
+    };
+  in {
+    nixosConfigurations = {
+      x86_64 = nixos-system "x86_64-linux";
+    };
+
+  };
+}
---
+++ module/home-manager.nix
@@ -0,0 +1,183 @@
+{pkgs, ...}: let
+
+i3_mod = "Mod1";
+
+in {
+
+  # add home-manager user settings here
+  home.packages = with pkgs; 
+    [
+      git
+      neovim
+    ];
+  home.stateVersion = "24.05";
+
+  programs.kitty = {
+    enable = true;
+    theme = "Material Dark";
+  };
+
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+	Locked = true;
+	Cryptomining = true;
+	Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+      DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+      SearchBar = "unified"; # alternative: "separate"
+      /* ---- EXTENSIONS ---- */
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = {
+        "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        # uBlock Origin:
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Privacy Badger:
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+	# Bitwarden:
+	"{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+      };
+      
+      Preferences = { 
+        "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+        "extensions.pocket.enabled" = false;
+        "extensions.screenshots.disabled" = true;
+        "browser.topsites.contile.enabled" = false;
+        "browser.formfill.enable" = false;
+        "browser.search.suggest.enabled" = false;
+        "browser.search.suggest.enabled.private" = false;
+        "browser.urlbar.suggest.searches" = false;
+        "browser.urlbar.showSearchSuggestionsFirst" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+      };
+      
+    };
+  };
+
+  programs.i3status = {
+    enable = true;
+
+    modules = {
+      ipv6.enable = false;
+      "wireless _first_".enable = false;
+      "battery all".enable = false;
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    theme = "glue_pro_blue";
+  };
+
+  xsession.windowManager.i3 = {
+    enable = true;
+
+    config = {
+      bars = [
+        {
+          position = "bottom";
+	  statusCommand = "${pkgs.i3status}/bin/i3status";
+          colors = {
+            background = "#282828";
+	    statusline = "#d4be98";
+          };
+        }
+      ];
+
+      keybindings = {
+        "${i3_mod}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+        "${i3_mod}+Shift+q" = "kill";
+
+        "${i3_mod}+d" = "exec --no-startup-id ${pkgs.rofi}/bin/rofi -modi drun -show drun";
+
+        "${i3_mod}+h" = "focus left";
+        "${i3_mod}+j" = "focus down";
+        "${i3_mod}+k" = "focus up";
+        "${i3_mod}+l" = "focus right";
+
+        "${i3_mod}+Shift+h" = "move left";
+        "${i3_mod}+Shift+j" = "move down";
+        "${i3_mod}+Shift+k" = "move up";
+        "${i3_mod}+Shift+l" = "move right";
+
+        "${i3_mod}+b" = "split h; exec notify-send 'Tile Horizontally'";
+        "${i3_mod}+v" = "split v; exec notify-send 'Tile Vertically'";
+        "${i3_mod}+f" = "fullscreen toggle";
+
+        "${i3_mod}+s" = "layout stacking";
+        "${i3_mod}+w" = "layout tabbed";
+        "${i3_mod}+e" = "layout toggle split";
+
+        "${i3_mod}+Shift+space" = "floating toggle";
+        "${i3_mod}+space" = "focus mode_toggle";
+
+        "${i3_mod}+a" = "focus parent";
+
+        "${i3_mod}+Shift+minus" = "move scratchpad";
+        "${i3_mod}+minus" = "scratchpad show";
+
+        "${i3_mod}+1" = "workspace number 1";
+        "${i3_mod}+2" = "workspace number 2";
+        "${i3_mod}+3" = "workspace number 3";
+        "${i3_mod}+4" = "workspace number 4";
+        "${i3_mod}+5" = "workspace number 5";
+        "${i3_mod}+6" = "workspace number 6";
+        "${i3_mod}+7" = "workspace number 7";
+        "${i3_mod}+8" = "workspace number 8";
+        "${i3_mod}+9" = "workspace number 9";
+        "${i3_mod}+0" = "workspace number 10";
+
+        "${i3_mod}+Shift+1" = "move container to workspace number 1";
+        "${i3_mod}+Shift+2" = "move container to workspace number 2";
+        "${i3_mod}+Shift+3" = "move container to workspace number 3";
+        "${i3_mod}+Shift+4" = "move container to workspace number 4";
+        "${i3_mod}+Shift+5" = "move container to workspace number 5";
+        "${i3_mod}+Shift+6" = "move container to workspace number 6";
+        "${i3_mod}+Shift+7" = "move container to workspace number 7";
+        "${i3_mod}+Shift+8" = "move container to workspace number 8";
+        "${i3_mod}+Shift+9" = "move container to workspace number 9";
+        "${i3_mod}+Shift+0" = "move container to workspace number 10";
+
+        "${i3_mod}+Shift+c" = "reload";
+        "${i3_mod}+Shift+r" = "restart";
+        "${i3_mod}+Shift+e" = "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
+
+        "${i3_mod}+r" = "mode resize";
+      };
+    };
+  };
+
+  programs.gh.enable = true;
+
+}
